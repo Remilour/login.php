55491adc945a677bef71727eb5972f208e403617
Remilour <111240295+Remilour@users.noreply.github.com>
2022-08-20 08:20:57 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan CSS Saya</title>
+    <link rel="stylesheet" href="style.css" >
+</head>
+<body>
+
+<form action="" method="get" class="box">
+    <h1>LOGIN</h1>
+    <label>
+        <input type="text" name="usename" placeholder="Username" autocomplete="off" autofocus/>
+    </label>
+    <label>
+        <input type="password" name="pass" placeholder="Password" required>
+    </label>
+    <button type="submit" name="submit">Login</button>
+    <a href="registrasi.php">Registrasi</a>
+</form>    
+</body>
+</html>
